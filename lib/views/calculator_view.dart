7021f3baa4885f03965222e29a368b2fcc2156c8
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-10-22 21:17:50 +0530

    new changes done

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorView extends StatelessWidget {
@@ -5,25 +6,101 @@ class CalculatorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.white,
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        children: [
+          const DisplayTextFiels(
+            hint: 'Enter First Number',
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const DisplayTextFiels(
+            hint: 'Enter Second Number',
+          ),
+          const SizedBox(
+            height: 45,
+          ),
+          const Text(
+            '0',
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
             ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.white,
-                
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(
+                  CupertinoIcons.add,
+                ),
               ),
-            ),
-            hintText: 'Enter number',
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(
+                  CupertinoIcons.minus,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(
+                  CupertinoIcons.multiply,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(
+                  CupertinoIcons.divide,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DisplayTextFiels extends StatelessWidget {
+  final String? hint;
+  const DisplayTextFiels({
+    super.key,
+    required this.hint,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autofocus: true,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: 3.0,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
           ),
         ),
-      ],
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: 3.0,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        hintText: hint,
+        hintStyle: const TextStyle(
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
